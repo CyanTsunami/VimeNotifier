ba670fa06122992d041bc61c94aecc6fec0feaa7
coroutined <coroutined@gmail.com>
2023-05-03 15:57:38 +0700

    Created UI-app for game notifications.

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace VimeNotifier {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameNotifyGroupBox = new System.Windows.Forms.GroupBox();
+            this.setDefaultGameSoundButton = new System.Windows.Forms.Button();
+            this.setOwnGameSoundButton = new System.Windows.Forms.Button();
+            this.gameSoundLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serverComboBox = new System.Windows.Forms.ComboBox();
+            this.chatNotifyGroupBox = new System.Windows.Forms.GroupBox();
+            this.setDefaultChatSoundButton = new System.Windows.Forms.Button();
+            this.setOwnChatSoundButton = new System.Windows.Forms.Button();
+            this.chatSoundLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ChatFlagCheckBox = new System.Windows.Forms.CheckBox();
+            this.DMFlagCheckBox = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.starterButton = new System.Windows.Forms.Button();
+            this.gameNotifyGroupBox.SuspendLayout();
+            this.chatNotifyGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameNotifyGroupBox
+            // 
+            this.gameNotifyGroupBox.Controls.Add(this.setDefaultGameSoundButton);
+            this.gameNotifyGroupBox.Controls.Add(this.setOwnGameSoundButton);
+            this.gameNotifyGroupBox.Controls.Add(this.gameSoundLabel);
+            this.gameNotifyGroupBox.Controls.Add(this.label6);
+            this.gameNotifyGroupBox.Location = new System.Drawing.Point(14, 206);
+            this.gameNotifyGroupBox.Name = "gameNotifyGroupBox";
+            this.gameNotifyGroupBox.Size = new System.Drawing.Size(288, 88);
+            this.gameNotifyGroupBox.TabIndex = 3;
+            this.gameNotifyGroupBox.TabStop = false;
+            this.gameNotifyGroupBox.Text = "Игровые уведомления";
+            // 
+            // setDefaultGameSoundButton
+            // 
+            this.setDefaultGameSoundButton.Location = new System.Drawing.Point(138, 48);
+            this.setDefaultGameSoundButton.Name = "setDefaultGameSoundButton";
+            this.setDefaultGameSoundButton.Size = new System.Drawing.Size(123, 23);
+            this.setDefaultGameSoundButton.TabIndex = 7;
+            this.setDefaultGameSoundButton.Text = "Сбросить";
+            this.setDefaultGameSoundButton.UseVisualStyleBackColor = true;
+            this.setDefaultGameSoundButton.Click += new System.EventHandler(this.setDefaultGameSoundButton_Click);
+            // 
+            // setOwnGameSoundButton
+            // 
+            this.setOwnGameSoundButton.Location = new System.Drawing.Point(9, 48);
+            this.setOwnGameSoundButton.Name = "setOwnGameSoundButton";
+            this.setOwnGameSoundButton.Size = new System.Drawing.Size(123, 23);
+            this.setOwnGameSoundButton.TabIndex = 6;
+            this.setOwnGameSoundButton.Text = "Выбрать свой звук...";
+            this.setOwnGameSoundButton.UseVisualStyleBackColor = true;
+            this.setOwnGameSoundButton.Click += new System.EventHandler(this.setOwnGameSoundButton_Click);
+            // 
+            // gameSoundLabel
+            // 
+            this.gameSoundLabel.Location = new System.Drawing.Point(39, 27);
+            this.gameSoundLabel.Name = "gameSoundLabel";
+            this.gameSoundLabel.Size = new System.Drawing.Size(222, 13);
+            this.gameSoundLabel.TabIndex = 5;
+            this.gameSoundLabel.Text = "...";
+            this.gameSoundLabel.TextChanged += new System.EventHandler(this.soundLabel_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Звук:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Сервер:";
+            // 
+            // serverComboBox
+            // 
+            this.serverComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.serverComboBox.FormattingEnabled = true;
+            this.serverComboBox.Items.AddRange(new object[] {
+            "Minigames",
+            "CivCraft",
+            "Vime",
+            "Explore",
+            "Discover",
+            "Flair",
+            "Empire",
+            "Wurst",
+            "Hoden"});
+            this.serverComboBox.Location = new System.Drawing.Point(72, 14);
+            this.serverComboBox.Name = "serverComboBox";
+            this.serverComboBox.Size = new System.Drawing.Size(139, 21);
+            this.serverComboBox.TabIndex = 0;
+            this.serverComboBox.SelectedIndexChanged += new System.EventHandler(this.serverComboBox_SelectedIndexChanged);
+            // 
+            // chatNotifyGroupBox
+            // 
+            this.chatNotifyGroupBox.Controls.Add(this.setDefaultChatSoundButton);
+            this.chatNotifyGroupBox.Controls.Add(this.setOwnChatSoundButton);
+            this.chatNotifyGroupBox.Controls.Add(this.chatSoundLabel);
+            this.chatNotifyGroupBox.Controls.Add(this.label3);
+            this.chatNotifyGroupBox.Controls.Add(this.ChatFlagCheckBox);
+            this.chatNotifyGroupBox.Controls.Add(this.DMFlagCheckBox);
+            this.chatNotifyGroupBox.Location = new System.Drawing.Point(14, 80);
+            this.chatNotifyGroupBox.Name = "chatNotifyGroupBox";
+            this.chatNotifyGroupBox.Size = new System.Drawing.Size(288, 120);
+            this.chatNotifyGroupBox.TabIndex = 2;
+            this.chatNotifyGroupBox.TabStop = false;
+            this.chatNotifyGroupBox.Text = "Уведомления в чате";
+            // 
+            // setDefaultChatSoundButton
+            // 
+            this.setDefaultChatSoundButton.Location = new System.Drawing.Point(138, 75);
+            this.setDefaultChatSoundButton.Name = "setDefaultChatSoundButton";
+            this.setDefaultChatSoundButton.Size = new System.Drawing.Size(123, 23);
+            this.setDefaultChatSoundButton.TabIndex = 5;
+            this.setDefaultChatSoundButton.Text = "Сбросить";
+            this.setDefaultChatSoundButton.UseVisualStyleBackColor = true;
+            this.setDefaultChatSoundButton.Click += new System.EventHandler(this.setDefaultChatSoundButton_Click);
+            // 
+            // setOwnChatSoundButton
+            // 
+            this.setOwnChatSoundButton.Location = new System.Drawing.Point(9, 75);
+            this.setOwnChatSoundButton.Name = "setOwnChatSoundButton";
+            this.setOwnChatSoundButton.Size = new System.Drawing.Size(123, 23);
+            this.setOwnChatSoundButton.TabIndex = 4;
+            this.setOwnChatSoundButton.Text = "Выбрать свой звук...";
+            this.setOwnChatSoundButton.UseVisualStyleBackColor = true;
+            this.setOwnChatSoundButton.Click += new System.EventHandler(this.setOwnSoundButton_Click);
+            // 
+            // chatSoundLabel
+            // 
+            this.chatSoundLabel.Location = new System.Drawing.Point(39, 23);
+            this.chatSoundLabel.Name = "chatSoundLabel";
+            this.chatSoundLabel.Size = new System.Drawing.Size(222, 13);
+            this.chatSoundLabel.TabIndex = 3;
+            this.chatSoundLabel.Text = "...";
+            this.chatSoundLabel.TextChanged += new System.EventHandler(this.soundLabel_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Звук:";
+            // 
+            // ChatFlagCheckBox
+            // 
+            this.ChatFlagCheckBox.AutoSize = true;
+            this.ChatFlagCheckBox.Location = new System.Drawing.Point(140, 46);
+            this.ChatFlagCheckBox.Name = "ChatFlagCheckBox";
+            this.ChatFlagCheckBox.Size = new System.Drawing.Size(124, 17);
+            this.ChatFlagCheckBox.TabIndex = 3;
+            this.ChatFlagCheckBox.Text = "Упоминание в чате";
+            this.ChatFlagCheckBox.UseVisualStyleBackColor = true;
+            this.ChatFlagCheckBox.CheckedChanged += new System.EventHandler(this.FlagCheckBox_CheckedChanged);
+            // 
+            // DMFlagCheckBox
+            // 
+            this.DMFlagCheckBox.AutoSize = true;
+            this.DMFlagCheckBox.Location = new System.Drawing.Point(9, 46);
+            this.DMFlagCheckBox.Name = "DMFlagCheckBox";
+            this.DMFlagCheckBox.Size = new System.Drawing.Size(125, 17);
+            this.DMFlagCheckBox.TabIndex = 2;
+            this.DMFlagCheckBox.Text = "Личные сообщения";
+            this.DMFlagCheckBox.UseVisualStyleBackColor = true;
+            this.DMFlagCheckBox.CheckedChanged += new System.EventHandler(this.FlagCheckBox_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(16, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Игрок:";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(73, 46);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(16, 13);
+            this.usernameLabel.TabIndex = 9;
+            this.usernameLabel.Text = "...";
+            // 
+            // starterButton
+            // 
+            this.starterButton.Location = new System.Drawing.Point(217, 14);
+            this.starterButton.Name = "starterButton";
+            this.starterButton.Size = new System.Drawing.Size(79, 23);
+            this.starterButton.TabIndex = 1;
+            this.starterButton.Text = "Включить";
+            this.starterButton.UseVisualStyleBackColor = true;
+            this.starterButton.Click += new System.EventHandler(this.starterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 306);
+            this.Controls.Add(this.starterButton);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chatNotifyGroupBox);
+            this.Controls.Add(this.serverComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gameNotifyGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "VimeNotifier";
+            this.gameNotifyGroupBox.ResumeLayout(false);
+            this.gameNotifyGroupBox.PerformLayout();
+            this.chatNotifyGroupBox.ResumeLayout(false);
+            this.chatNotifyGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox gameNotifyGroupBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox serverComboBox;
+        private System.Windows.Forms.GroupBox chatNotifyGroupBox;
+        private System.Windows.Forms.CheckBox ChatFlagCheckBox;
+        private System.Windows.Forms.CheckBox DMFlagCheckBox;
+        private System.Windows.Forms.Label chatSoundLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button setOwnChatSoundButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Button setDefaultChatSoundButton;
+        private System.Windows.Forms.Button starterButton;
+        private System.Windows.Forms.Button setDefaultGameSoundButton;
+        private System.Windows.Forms.Button setOwnGameSoundButton;
+        private System.Windows.Forms.Label gameSoundLabel;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,278 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Media;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VimeNotifier {
+    public partial class Form1: Form {
+        private Properties.Settings settings = Properties.Settings.Default;
+        private Regex userLineRegex, logRemoveRegex, DMRegex, chatRegex;
+        private BackgroundWorker chatWorker, apiWorker;
+        private SoundPlayer player;
+        private string logsPath, chatSoundPath, gameSoundPath, username;
+
+        public Form1() {
+            InitializeComponent();
+            string systemUsername = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            logsPath = "C:\\Users\\" + systemUsername.Substring(systemUsername.LastIndexOf('\\') + 1) +
+                       "\\AppData\\Roaming\\.vimeworld\\{0}\\logs\\latest.log";
+            chatSoundLabel.Text = chatSoundPath = !File.Exists(settings.chatSoundPath) ?
+                settings.chatSoundPath :
+                "C:\\Windows\\Media\\Windows Proximity Notification.wav";
+            gameSoundLabel.Text = gameSoundPath = !File.Exists(settings.gameSoundPath) ?
+                settings.gameSoundPath :
+                "C:\\Windows\\Media\\Windows Proximity Notification.wav";
+            DMFlagCheckBox.Checked = settings.DMFlagCheckBox;
+            ChatFlagCheckBox.Checked = settings.ChatFlagCheckBox;
+            serverComboBox.Text = settings.serverComboBox;
+
+            userLineRegex = new Regex(
+                "^\\[\\d{2}:\\d{2}:\\d{2}\\]( \\[[\\w .\\/]*\\])*: " +
+                "Setting user: \\w{3,16}");
+            logRemoveRegex = new Regex(
+                "^\\[\\d{2}:\\d{2}:\\d{2}\\]( \\[[\\w .\\/]*\\])*: \\[CHAT\\] " +
+                "(\\(Всем\\) |<.{1,}> )?(\\[.{1,4}\\] )?");
+            DMRegex = new Regex(
+                "^\\[\\d{2}:\\d{2}:\\d{2}\\]( \\[[\\w .\\/]*\\])*: \\[CHAT\\] " +
+                "\\[[\\w\\d]{3,16} -> Вы\\] .*$");
+            chatRegex = new Regex(
+                "^\\[\\d{2}:\\d{2}:\\d{2}\\]( \\[[\\w .\\/]*\\])*: \\[CHAT\\]" +
+                "( \\(Всем\\)| <.{1,5}>)?( \\[.{1,4}\\])? [\\w\\d]{3,16}: .*$");
+
+            chatWorker = new BackgroundWorker {
+                WorkerSupportsCancellation = true
+            };
+            chatWorker.DoWork += new DoWorkEventHandler(chatHandler);
+            apiWorker = new BackgroundWorker {
+                WorkerSupportsCancellation = true
+            };
+            apiWorker.DoWork += new DoWorkEventHandler(apiHandler);
+            player = new SoundPlayer(chatSoundPath);
+
+            settings.chatSoundPath = chatSoundPath;
+            settings.gameSoundPath = gameSoundPath;
+            settings.Save();
+        }
+
+        public IEnumerable<string> ReadLines(StreamReader reader) {
+            string line;
+            while((line = reader.ReadLine()) != null) {
+                yield return line;
+            }
+        }
+
+        public string getUsername(string filepath) {
+            Stream stream = new FileStream(filepath, FileMode.Open,
+                                           FileAccess.Read, FileShare.ReadWrite);
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            string result = string.Empty;
+            foreach(string line in ReadLines(reader)) {
+                if(userLineRegex.Match(line).Success) {
+                    int index = line.IndexOf("Setting user:");
+                    result = line.Substring(index + 14);
+                    break;
+                }
+            }
+            reader.Close();
+            stream.Close();
+            return result;
+        }
+
+        private void FlagCheckBox_CheckedChanged(object sender, EventArgs e) {
+            CheckBox box = (CheckBox)sender;
+            settings[box.Name] = box.Checked;
+            settings.Save();
+        }
+
+        private void soundLabel_TextChanged(object sender, EventArgs e) {
+            Label label = (Label)sender;
+            label.Text = label.Text.Substring(label.Text.LastIndexOf('\\') + 1);
+        }
+
+        private void setDefaultChatSoundButton_Click(object sender, EventArgs e) {
+            chatSoundLabel.Text = chatSoundPath = settings.chatSoundPath = 
+                "C:\\Windows\\Media\\Windows Proximity Notification.wav";
+            settings.Save();
+        }
+
+        private void setDefaultGameSoundButton_Click(object sender, EventArgs e) {
+            gameSoundLabel.Text = gameSoundPath = settings.gameSoundPath =
+                "C:\\Windows\\Media\\Windows Proximity Notification.wav";
+            settings.Save();
+        }
+
+        public dynamic GetJsonResponse(HttpClient client, string url, ref int code) {
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            dynamic obj;
+            if(!response.IsSuccessStatusCode) {
+                obj = response.Content != null ?
+                    JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result) : 
+                    new JObject();
+                code = obj.First != null ? 
+                    ((int)obj.First["error"]["error_code"]) : -3;
+                return obj;
+            }
+            obj = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result);
+            code = 0;
+            return obj;
+        }
+
+        public void safeRestore() {
+            if(!InvokeRequired) {
+                chatWorker.CancelAsync();
+
+                starterButton.Text = chatWorker.IsBusy ? "Включить" : "Выключить";
+                chatNotifyGroupBox.Enabled = chatWorker.IsBusy;
+                gameNotifyGroupBox.Enabled = serverComboBox.Text == "Minigames" && chatWorker.IsBusy;
+            } else {
+                Invoke(new Action(() => safeRestore()));
+            }
+        }
+
+        private void chatHandler(object sender, DoWorkEventArgs e) {
+            Stream stream = new FileStream((string)e.Argument, FileMode.Open,
+                                           FileAccess.Read, FileShare.ReadWrite);
+            stream.Seek(0, SeekOrigin.End);
+
+            using(StreamReader reader = new StreamReader(stream, Encoding.UTF8)) {
+                while(!chatWorker.CancellationPending) {
+                    foreach(string templine in ReadLines(reader)) {
+                        if((ChatFlagCheckBox.Checked && chatRegex.Match(templine).Success)) {
+                            string line = logRemoveRegex.Replace(templine, "");
+                            string[] parts = line.Split(new char[] {':'}, 2);
+                            if(parts[1].ToLower().Contains(username)) {
+                                player.SoundLocation = chatSoundPath;
+                                player.Play();
+                            }
+                        } else if(DMFlagCheckBox.Checked && DMRegex.Match(templine).Success) {
+                            player.SoundLocation = chatSoundPath;
+                            player.Play();
+                        }
+                    }
+                    Thread.Sleep(500);
+                }
+            }
+            stream.Close();
+        }
+
+        private void apiHandler(object sender, DoWorkEventArgs e) {
+            using(HttpClient client = new HttpClient()) {
+                int code = 0;
+                dynamic oldJson = GetJsonResponse(client, 
+                    "https://api.vimeworld.com/user/name/" + username, ref code);
+                if(code != 0) {
+                    safeRestore();
+                    MessageBox.Show("Ошибка обращения к API. Код ошибки: " + code.ToString(),
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                int id = ((int)oldJson[0]["id"]);
+                oldJson = GetJsonResponse(client, "https://api.vimeworld.com/user/" + id.ToString() +
+                    "/session", ref code);
+                if(code != 0) {
+                    safeRestore();
+                    MessageBox.Show("Ошибка обращения к API. Код ошибки: " + code.ToString(),
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                while(!apiWorker.CancellationPending) {
+                    Thread.Sleep(2000);
+                    dynamic newJson = GetJsonResponse(client, "https://api.vimeworld.com/user/" + id.ToString() +
+                        "/session", ref code);
+                    if(code != 0) {
+                        safeRestore();
+                        MessageBox.Show("Ошибка обращения к API. Код ошибки: " + code.ToString(),
+                            "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    string newMsg = newJson["online"]["message"].ToString();
+                    string oldMsg = oldJson["online"]["message"].ToString();
+                    if(newMsg != oldMsg && newMsg.ToString() != "Находится в Лобби") {
+                        player.SoundLocation = gameSoundPath;
+                        player.Play();
+                    }
+                    oldJson = newJson;
+                }
+            }
+        }
+
+        private void setOwnSoundButton_Click(object sender, EventArgs e) {
+            using(OpenFileDialog dialog = new OpenFileDialog()) {
+                dialog.InitialDirectory = chatSoundPath.Remove(chatSoundPath.LastIndexOf('\\'));
+                dialog.Filter = ".wav файлы (*.wav)|*.wav";
+                dialog.FilterIndex = 0;
+                dialog.RestoreDirectory = true;
+                if(dialog.ShowDialog() == DialogResult.OK) {
+                    chatSoundLabel.Text = settings.chatSoundPath = chatSoundPath = dialog.FileName;
+                    settings.Save();
+                }
+            }
+        }
+
+        private void setOwnGameSoundButton_Click(object sender, EventArgs e) {
+            using(OpenFileDialog dialog = new OpenFileDialog()) {
+                dialog.InitialDirectory = chatSoundPath.Remove(chatSoundPath.LastIndexOf('\\'));
+                dialog.Filter = ".wav файлы (*.wav)|*.wav";
+                dialog.FilterIndex = 0;
+                dialog.RestoreDirectory = true;
+                if(dialog.ShowDialog() == DialogResult.OK) {
+                    gameSoundLabel.Text = settings.gameSoundPath = gameSoundPath = dialog.FileName;
+                    settings.Save();
+                }
+            }
+        }
+
+        private void serverComboBox_SelectedIndexChanged(object sender, EventArgs e) {
+            gameNotifyGroupBox.Enabled = serverComboBox.Text == "Minigames";
+            settings.serverComboBox = serverComboBox.Text;
+            settings.Save();
+        }
+
+        private async void starterButton_Click(object sender, EventArgs e) {
+            string path = string.Format(logsPath, serverComboBox.Text.ToLower());
+            if(!File.Exists(path)) {
+                MessageBox.Show("Файл не существует", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            username = getUsername(path);
+            if(username == string.Empty) {
+                MessageBox.Show("Не удалось найти никнейм", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            usernameLabel.Text = username;
+            username = username.ToLower();
+
+            starterButton.Text = chatWorker.IsBusy ? "Включить" : "Выключить";
+            starterButton.Enabled = false;
+            serverComboBox.Enabled = chatNotifyGroupBox.Enabled = chatWorker.IsBusy;
+            gameNotifyGroupBox.Enabled = serverComboBox.Text == "Minigames" && chatWorker.IsBusy;
+            if(chatWorker.IsBusy) {
+                if(serverComboBox.Text == "Minigames") {
+                    apiWorker.CancelAsync();
+                }
+                chatWorker.CancelAsync();
+            } else {
+                if(serverComboBox.Text == "Minigames") {
+                    apiWorker.RunWorkerAsync();
+                }
+                chatWorker.RunWorkerAsync(path);
+            }
+            await Task.Delay(2000);
+            starterButton.Enabled = true;
+        }
+    }
+}
